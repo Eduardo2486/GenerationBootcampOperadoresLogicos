e5f938736f1dd9be167ad6a774944432dda35c54
Eduardo2486 <eduardo0137@protonmail.com>
2021-07-15 17:38:04 -0500

    Logic operators java

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		byte smallNumber = 122;
+		byte anotherSmallnumber = 122;
+		int sum = smallNumber + anotherSmallnumber;
+		
+		long number1 = 1000;
+		int number2 = (int) number1;
+		float number3 = number2;
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(sum);
+	}
+
+}
